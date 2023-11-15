d643c0e542df53831ae7c84c2e7e5042cce9cfdb
Luis <llopezderueda0480@ceuandalucia.es>
2023-11-15 17:51:22 +0100

    Examen terminado todo

---
+++ Swing_llopezderueda0480/src/examen/gui/TableModel.java
@@ -0,0 +1,71 @@
+package examen.gui;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import examen.modelo.Coche;
+
+public class TableModel extends AbstractTableModel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6112205562326517127L;
+	private List<Coche> listaCoches;
+
+	public List<Coche> getListaCoches() {
+		return listaCoches;
+	}
+
+	public void setListaCoches(List<Coche> listaCoches) {
+		this.listaCoches = listaCoches;
+	}
+
+	public TableModel() {
+		listaCoches = new ArrayList<>();
+	}
+
+	@Override
+	public int getRowCount() {
+
+		return listaCoches.size();
+	}
+
+	@Override
+	public int getColumnCount() {
+
+		return 4;
+	}
+
+	@Override
+	public String getColumnName(int column) {
+		if (column == 0) {
+			return "Marca";
+		} else if (column == 1) {
+			return "Modelo";
+		} else if (column == 2) {
+			return "Úlrtima revisión";
+		} else
+			return "Antigüedad";
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		Coche coche = listaCoches.get(rowIndex);
+		if (columnIndex == 0) {
+			return coche.getMarca();
+		}
+		if (columnIndex == 1) {
+			return coche.getModelo();
+		}
+		if (columnIndex == 2) {
+			return coche.getFechaUltimaRevision().format(formato);
+		} else
+			return coche.getAntiguedad();
+
+	}
+
+}
